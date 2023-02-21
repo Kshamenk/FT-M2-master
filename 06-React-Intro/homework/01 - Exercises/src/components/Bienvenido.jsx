e84013d050c46328f4673505fb35ef0a331b25db
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-02-21 10:13:35 -0300

    101321022023

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Kchi";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
-  // el código de tu componente acá
-  return;
+  const listItems = techSkills.map((skill) => {
+    return <li>{skill}</li>;
+  });
+
+  return (
+    <div>
+      <h1>Bienvenido</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((skill) => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+      <Botones alerts={alerts} />   
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Botones extends React.Component {
+  render() {
+    const { m1, m2 } = this.props.alerts;  //se lo paso como propss
+
+    return (
+      <div>
+        <button onClick={() => alert(m1)}>Módulo 1</button>
+        <button onClick={() => alert(m2)}>Módulo 2</button>
+      </div>
+    );
+  }
+}
+
+export default Botones;
