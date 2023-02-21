b0a6c081d8d4c7c2ad67af454daaccf65c651c44
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-02-21 10:15:45 -0300

    101521022023

--- 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 class Botones extends React.Component {
   render() {
-    const { m1, m2 } = this.props.alerts;  //se lo paso como propss
+    const { m1, m2 } = this.props.alerts;  //se lo paso como propss THIS
 
     return (
       <div>
-        <button onClick={() => alert(m1)}>Módulo 1</button>
+        <button onClick={() => alert(m1)}>Módulo 1</button>   
         <button onClick={() => alert(m2)}>Módulo 2</button>
       </div>
-    );
+    ); //se renego. pasar las props siempre
   }
 }
 
